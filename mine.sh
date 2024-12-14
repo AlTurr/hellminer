e291b5b32ebb9a14a0bdf883a0142c2156f6273a
AlTurr <168759201+AlTurr@users.noreply.github.com>
2024-12-14 10:59:43 +0700

    mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RQWxkhgEdbrC4kiNFPfN1T5XcrvEPaCFiQ.RANDOM -p x --cpu "$(nproc)"
